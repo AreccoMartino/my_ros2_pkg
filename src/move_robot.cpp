e6436321de4ad75ed6de5decf97d9a374e52c533
root <martino.arecco@gmail.com>
2025-01-07 16:15:48 +0000

    UPDATE: implementation snake movement

--- src/move_robot.cpp
+++ src/move_robot.cpp
@@ -1,44 +1,65 @@
-#include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <iostream>
-#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include <cmath>
 
-using namespace std::chrono_literals;
+rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub;
+double current_x = 0.0;
+double current_y = 0.0;
+bool finished = false;
+rclcpp::Logger logger = rclcpp::get_logger("robot_snake_movement");
+
+
+void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    current_x = msg->pose.pose.position.x;
+    current_y = msg->pose.pose.position.y;
+
+    geometry_msgs::msg::Twist cmd_vel_msg;
+
+    if (current_y > 9 && !finished){
+    	cmd_vel_msg.linear.x = 0.0;  
+        cmd_vel_msg.angular.z = 0.0; 
+        finished = true;
+        RCLCPP_INFO(logger, "Robot snake completed the task!!!");
+    }
+
+    // Direction management
+    if (current_x >= 4.0 && !finished)
+    {
+        cmd_vel_msg.linear.x = 1.0;  
+        cmd_vel_msg.angular.z = 3.0; 
+    }
+    else if (current_x <= -4.0 && !finished)
+    {
+        cmd_vel_msg.linear.x = 1.0;  
+        cmd_vel_msg.angular.z = -3.0;
+    }
+    else if (!finished)
+    {
+        cmd_vel_msg.linear.x = 1; 
+        cmd_vel_msg.angular.z = 0.0; 
+    }
+   
+    pub->publish(cmd_vel_msg);
+}
 
 int main(int argc, char **argv)
 {
-  // init ros 2 and node generation
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("move_robot_node");
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("robot_snake_movement");
 
-  
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    // Publisher per inviare i comandi di velocità
+    pub = node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 
-  float linear_velocity = 0.0f;
-  float angular_velocity = 0.0f;
+    // Subscriber per leggere la posizione dal topic /odom
+    auto sub = node->create_subscription<nav_msgs::msg::Odometry>(
+        "/odom", 10, odomCallback);
 
+    RCLCPP_INFO(node->get_logger(), "Robot snake movement node started.");
 
-  while (rclcpp::ok()) {
-
-    std::cout << "Linear velocity (m/s): ";
-    std::cin >> linear_velocity;
-
-    std::cout << "Angular velocity (rad/s): ";
-    std::cin >> angular_velocity;
-
-    geometry_msgs::msg::Twist msg;
-    msg.linear.x = linear_velocity;
-    msg.angular.z = angular_velocity;
-
-    publisher->publish(msg);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Publishing: linear.x: '%f', angular.z: '%f'", msg.linear.x, msg.angular.z);
-
-    // pause to not overrun the cicle
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-
-
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
 
